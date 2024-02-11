d95263b7a39b5f21525a2fde23da43521dda650e
FlerBuster <96306683+FlerBuster@users.noreply.github.com>
2024-02-11 20:35:13 +0100

    ich tweake

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// 11.2.2024 -> Ich tweake
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
